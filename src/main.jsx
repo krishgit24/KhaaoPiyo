70598b34c83fcc7bd2417a1603097328023c3b85
Krish Dambir <krishdambir2718@gmail.com>
2025-08-25 00:21:25 +0530

    more changes of token

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./Context/UserContext.jsx";
-import { CartProvider } from "./Context/CartContext.jsx";
 import { FoodProvider } from "./Context/FoodContext.jsx";
+import { CartProvider } from "./Context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
-      <FoodProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FoodProvider>
-    </UserProvider>
+    <BrowserRouter>
+      <UserProvider>
+        <FoodProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FoodProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
