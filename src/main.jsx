d8fa3059a5cb76ec1d24530fe598944bd618d466
Krish Dambir <krishdambir2718@gmail.com>
2025-08-24 22:05:40 +0530

    fix: router basename removed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
    <StrictMode>
     <FoodProvider>
-      <BrowserRouter basename="/KhaaoPiyo">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </FoodProvider>
